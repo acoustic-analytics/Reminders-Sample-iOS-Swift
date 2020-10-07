f961ea8149dfc9bf0b066ae821ad0dfd23e01fd9
Shridhar Damale <shridhar.damale@acoustic.co>
2020-10-07 12:05:03 +0530

    update encryption example

--- RemindersSwiftApp/Extensions/CryptographyExtensions.swift
+++ RemindersSwiftApp/Extensions/CryptographyExtensions.swift
@@ -13,9 +13,9 @@ import Foundation
 This file is provided as is and as sample only. You should implement your own extension NSString for encryption-decryption as per your security policy. You do not have to have these methods or categories. This is optional. If these methods are implemented in your application, Tealeaf SDK  would call these APIs before saving data to local storage. If you choose to implement these; please test the methods properly; make sure there is a faithful encryption and decryption of the JSON string passed in. SDK relies on your application's NSString ecryption decryption methods. SDK itself does not encrypt, decrypt or validate if these operations are successful. Hence carefully implement and test.
 */
 
-var keyStr = ""; //@"<please enter 32 byte long key here>";
-extension NSString {
-    public func encryptTealeafData() -> NSString?{
+var keyStr = ""; //@"<please enter 32 byte long key here>" For example: 0123456789abcdef>;
+@objc extension NSString {
+    @objc public func encryptTealeafData() -> NSString?{
         var encryptedString = self
         if( (keyStr.count > 0) && (self.length > 0) ){
             do {
@@ -32,7 +32,8 @@ extension NSString {
                                                                  &error) as Data? else {
                                                                     throw error!.takeRetainedValue() as Error
                 }
-                encryptedString = String(decoding: encryptedData, as: UTF8.self) as NSString? ?? ""
+                //encryptedString = String(decoding: encryptedData, as: UTF8.self) as NSString? ?? ""
+                encryptedString = encryptedData .base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters) as NSString
             } catch {
                 print(error)
             }
@@ -40,7 +41,7 @@ extension NSString {
         return encryptedString
     }
 
-    public func decryptTealeafData() -> NSString?{
+    @objc public func decryptTealeafData() -> NSString?{
         var decryptedString = self
         if( (keyStr.count > 0) && (self.length > 0) ){
             do {
